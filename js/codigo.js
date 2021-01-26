693f63d31bb9e1d25fac6f75397f362d9ad2f86c
Bruno <bruno259825@gmail.com>
2021-01-26 18:59:39 +0100

    ficheros add

---
+++ js/codigo.js
@@ -0,0 +1,4 @@
+"use strict";
+var oNotas= new oNotas();
+
+
---
+++ js/objetos.js
@@ -0,0 +1,40 @@
+class Notas{
+    constructor(){
+        this._notas=[];
+        this._usuarios=[];
+        this._usuarioGrupos=[];
+        this._grupos=[];
+    }
+
+    registrarUsuario(oUsuario){
+
+    }
+
+    modificarUsuario(oUsuario){
+
+    }
+
+    bajaUsuario(oUsuario){
+
+    }
+
+    altaNota(oNota){
+
+    }
+
+    modificarNota(oNota){
+
+    }
+
+    bajaNota(oNota){
+
+    }
+
+    altaGrupo(oGrupo){
+
+    }
+
+    bajaGrupo(oGrupo){
+
+    }
+}
