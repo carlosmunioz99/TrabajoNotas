a975bbc477aff1beb51395731376fbbe61d131e0
bruno <bruno259825@gmail.com>
2021-01-28 12:38:52 +0100

    arreglado el formulario registro

--- js/codigo.js
+++ js/codigo.js
@@ -3,15 +3,18 @@ var oNotas = new Notas();
 
 // Manejadores de eventos.
 let registrarUsuario = () => {
-  let vErrores = [];
   let bError = false;
 
   let sIdUsuario = frmRegistro.txtIdUsuario.value.trim();
   let oPatron = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{5,10}$/;
 
   if (!oPatron.test(sIdUsuario)) {
-    vErrores.push("- Formato de nombre de usuario incorrecto");
+    let $smallError=frmRegistro.txtIdUsuario.nextElementSibling;
+    $smallError.textContent="- Formato de nombre de usuario incorrecto";
     bError = true;
+  }else{
+    let $smallError=frmRegistro.txtIdUsuario.nextElementSibling;
+    $smallError.textContent="";
   }
 
 
@@ -19,22 +22,34 @@ let registrarUsuario = () => {
   oPatron = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]{2,50}$/;
 
   if (!oPatron.test(sNombre)) {
-    vErrores.push("- Formato de nombre incorrecto");
+    let $smallError=frmRegistro.txtNombre.nextElementSibling;
+    $smallError.textContent="- Formato de nombre incorrecto";
     bError = true;
+  }else{
+    let $smallError=frmRegistro.txtNombre.nextElementSibling;
+    $smallError.textContent="";
   }
 
   let sEmail = frmRegistro.txtMail.value.trim();
   oPatron = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
   if (!oPatron.test(sEmail)) {
-    vErrores.push("- Formato de email incorrecto");
+    let $smallError=frmRegistro.txtMail.nextElementSibling;
+    $smallError.textContent="- Formato de email incorrecto";
     bError = true;
+  }else{
+    let $smallError=frmRegistro.txtMail.nextElementSibling;
+    $smallError.textContent="";
   }
 
   let sPasword = frmRegistro.txtContraseña.value;
   oPatron = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{6,10}$/;
   if (!oPatron.test(sPasword)) {
-    vErrores.push("- Formato de password incorrecto");
+    let $smallError=frmRegistro.txtContraseña.nextElementSibling;
+    $smallError.textContent="- Formato de password incorrecto";
     bError = true;
+  }else{
+    let $smallError=frmRegistro.txtContraseña.nextElementSibling;
+    $smallError.textContent="";
   }
 
   if (!bError) {
@@ -42,33 +57,47 @@ let registrarUsuario = () => {
     let oUsuario = new Usuario(sIdUsuario,sPasword,sEmail,sNombre);
     
     if(oNotas.registrarUsuario(oUsuario)){
-        vErrores.push("Se ha registrado correctamente.");
+        alert("registrado correctamente");
     }else{
-        vErrores.push("No se ha podido registrar");
+      alert("nombre de usuario o email ya existen");
     }
 
   } 
 
-  let $cuerpoModal=document.querySelector("#modalError .modal-body");
-
-  for(let i=0;i<vErrores.length;i++){
-    let $nuevoP=document.createElement("p");
-    $nuevoP.textContent=vErrores[i];
-
-    $cuerpoModal.appendChild($nuevoP);
-  }
- 
-
-  $("#modalRegistro").hide();
-
-
-  $("#modalError").show();
 }
 
+let limpiarRegistro =()=>{
+  console.log("asd");
+  frmRegistro.reset();
+  let vSmallErrores=document.querySelectorAll("#modalRegistro small");
 
-//eventos
+  for(let i=0;i<vSmallErrores.length;i++){
+    vSmallErrores[i].textContent="";
+  }
+}
+
+let iniciarSesion=()=>{
+  let sNombreUsuario=frmLogin.txtNombreUsuario.value.trim();
+
+  if(oNotas.iniciarSesion()){
+    frmLogin.submit();
+  }
+  frmLogin
+}
+
+//EVENTOS
+
+//REGISTRO
 let $btnRegistrar = document.getElementById("btnRegistrarUsuario");
 
 $btnRegistrar.addEventListener("click", registrarUsuario);
 
-let $btnEntrarLogin = document.getElementById("");
+let $btnCerrarRegistro=document.querySelector("#modalRegistro button");
+
+$btnCerrarRegistro.addEventListener("click", limpiarRegistro);
+
+
+//LOGIN
+let $btnEntrarLogin = document.getElementById("btnLogin");
+
+$btnEntrarLogin.addEventListener("click", iniciarSesion);
