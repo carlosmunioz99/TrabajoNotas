85cd30992d1df4bf4962bd489f67a9be000d8012
carlosmunioz99 <61143085+carlosmunioz99@users.noreply.github.com>
2021-02-02 20:35:03 +0100

    CosasEnMiRama

--- js/codigo.js
+++ js/codigo.js
@@ -1,4 +1,373 @@
 "use strict";
-var oNotas= new oNotas();
+var oNotas = new Notas();
+//usuarios de prueba
+oNotas.registrarUsuario(new Usuario("pepe123","idvacs","pepe@gmail","Pepe"));
+oNotas.registrarUsuario(new Usuario("juan23","sfef","pepe@gmail","Jaunito"));
+oNotas.registrarUsuario(new Usuario("elmidas2","idvagfddfgcs","elmmidas@gmail","Mid"));
+oNotas.registrarUsuario(new Usuario("estesech","dfg","sech@gmail","Sech"));
+oNotas.registrarUsuario(new Usuario("carlos","cmr99","carlos@gmail","cmr1234"));
+
+// Manejadores de eventos.
+let registrarUsuario = () => {
+  let bError = false;
+
+  let sIdUsuario = frmRegistro.txtIdUsuario.value.trim();
+  let oPatron = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{5,30}$/;
+
+  if (!oPatron.test(sIdUsuario)) {
+    let $smallError=frmRegistro.txtIdUsuario.nextElementSibling;
+    $smallError.textContent="- El id debe estar compuesta por números o letras entre 5 y 30 caracteres.";
+    bError = true;
+  }else{
+    let $smallError=frmRegistro.txtIdUsuario.nextElementSibling;
+    $smallError.textContent="";
+  }
 
 
+  let sNombre = frmRegistro.txtNombre.value.trim();
+  oPatron = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,50}$/;
+
+  if (!oPatron.test(sNombre)) {
+    let $smallError=frmRegistro.txtNombre.nextElementSibling;
+    $smallError.textContent="- Formato de nombre incorrecto.";
+    bError = true;
+  }else{
+    let $smallError=frmRegistro.txtNombre.nextElementSibling;
+    $smallError.textContent="";
+  }
+
+  let sEmail = frmRegistro.txtMail.value.trim();
+  oPatron = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
+  if (!oPatron.test(sEmail)) {
+    let $smallError=frmRegistro.txtMail.nextElementSibling;
+    $smallError.textContent="- Formato de email incorrecto";
+    bError = true;
+  }else{
+    let $smallError=frmRegistro.txtMail.nextElementSibling;
+    $smallError.textContent="";
+  }
+
+  let sPasword = frmRegistro.txtContraseña.value;
+  oPatron = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{6,15}$/;
+  if (!oPatron.test(sPasword)) {
+    let $smallError=frmRegistro.txtContraseña.nextElementSibling;
+    $smallError.textContent="- La contraseña debe estar compuesta por números o letras, entre 6 y 15 caracteres.";
+    bError = true;
+  }else{
+    let $smallError=frmRegistro.txtContraseña.nextElementSibling;
+    $smallError.textContent="";
+  }
+
+  if (!bError) {
+
+    let oUsuario = new Usuario(sIdUsuario,sPasword,sEmail,sNombre);
+    let $mensaje=frmRegistro.firstElementChild;
+
+    $mensaje.classList.remove("ocultar");
+    if(oNotas.registrarUsuario(oUsuario)){
+      $mensaje.textContent="Registrado correctamente.";
+      $mensaje.classList.remove("alert-danger");
+      $mensaje.classList.add("alert-success");
+      generarTablaUsuarios();
+      frmRegistro.reset();
+      /*AÑADIR USUARIO AL COMBO MULTIPLE DE GRUPOS*/
+      let oLista = document.querySelector("#usuariosGrupos");
+      let oOpcion = document.createElement('option');
+      oOpcion.textContent = oUsuario.usuario;
+      oLista.appendChild(oOpcion);
+
+    }else{
+      $mensaje.textContent="Nombre de usuario o email ya existen";
+      $mensaje.classList.remove("alert-success");
+      $mensaje.classList.add("alert-danger");
+    }
+
+  } 
+
+}
+
+
+let limpiarRegistro =()=>{
+  
+  frmRegistro.reset();
+  let vSmallErrores=document.querySelectorAll("#modalRegistroUsuario small,#modalRegistroUsuario form> div:first-child");
+  
+  vSmallErrores[0].classList.add("ocultar");
+  for(let i=0;i<vSmallErrores.length;i++){
+    vSmallErrores[i].textContent="";
+  }
+}
+
+let borrarUsuario=(e)=>{
+  if(e.target.getAttribute("class")=="fondo-eliminar"){
+    let sNombreUsuario=e.target.getAttribute("data-id");
+    oNotas.bajaUsuario(sNombreUsuario);
+    generarTablaUsuarios();
+  }
+}
+
+
+let iniciarSesion=()=>{
+  
+  let sInfo="";
+  let $mensaje=frmLogin.querySelector(".alert");
+  $mensaje.textContent="";
+  let bError = false;
+  let vError=[];
+
+  let sNombreUsuario=frmLogin.txtNombreUsuario.value.trim();
+
+  let oPatron = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{5,10}$/;
+   
+
+  
+  if (!oPatron.test(sNombreUsuario)) {
+    vError.push("- Formato de nombre de usuario incorrecto.");
+    
+    bError = true;
+  }
+
+  let sPasword = frmLogin.txtContraseña.value;
+  oPatron = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ]{6,10}$/;
+  
+  if (!oPatron.test(sPasword)) {
+    
+    vError.push("- Formato de contraseña incorrecto.");
+    
+    bError = true;
+  }
+  
+  if(!bError){
+    if(oNotas.buscarUsuario(sNombreUsuario,sPasword)){
+
+      frmLogin.submit(); 
+  
+    }else{
+      vError.push("- Nombre de usuario o contraseña incorrectos.");
+      
+      bError=true;
+    }
+  }
+
+  if(bError){
+    vError.forEach(i=>{
+      let $p=document.createElement("p");
+      $p.textContent=i;
+      $mensaje.appendChild($p);
+    });
+    $mensaje.classList.remove("ocultar");
+    $mensaje.classList.add("alert-danger");
+  }
+  
+  
+}
+
+let limpiarLogin=()=>{
+  let $mensaje=frmLogin.querySelector(".alert");
+  $mensaje.textContent="";
+  $mensaje.classList.add("ocultar");
+  frmLogin.reset();
+}
+
+let generarTablaUsuarios=()=>{
+  
+  //vaciar tablas si es q hay
+  let $posibleTabla=document.querySelector("#contenedor-usuarios table");
+  if($posibleTabla!=null){
+    $posibleTabla.remove();
+  }
+
+  //creación de tabla
+  let $tabla=document.createElement("TABLE");
+  $tabla.classList.add("table");
+
+  let $thead=$tabla.createTHead();
+  let $row1=$thead.insertRow(-1);
+  $row1.insertCell(-1).textContent="ID";
+  $row1.insertCell(-1).textContent="NOMBRE";
+  $row1.insertCell(-1).textContent="EMAIL";
+  $row1.insertCell(-1).textContent="CONTRASEÑA";
+  $row1.insertCell(-1).textContent="EDITAR";
+  $row1.insertCell(-1).textContent="ELIMINAR";
+
+  //cuerpo de la tabla
+  let $tbody=$tabla.createTBody();
+
+  oNotas.generarFilasUsuarios($tbody);
+
+
+  let $contUsuarios=document.getElementById("contenedor-usuarios");
+
+
+  $contUsuarios.insertBefore($tabla,$contUsuarios.lastElementChild);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*GRUPOS*/
+
+/**RELLENAR COMBO MULTIPLE DE USUARIOS PARA GRUPOS*/
+
+let oLista = document.querySelector("#usuariosGrupos");
+let listaUsuarios = oNotas.getUsuarios();
+//console.log(listaUsuarios);
+for(let i=0;i<listaUsuarios.length;i++)
+{
+  let oOpcion = document.createElement('option');
+  oOpcion.textContent = listaUsuarios[i].usuario;
+  oLista.appendChild(oOpcion);
+}
+/*****************************************/
+
+
+function validarGrupos()
+{
+  let bValido = true;
+  let txtNombreGrupo = frmAñadirGrupo.txtNombreGrupo.value.trim();
+  //console.log(txtNombreGrupo);
+  let regExp = /^[0-9a-zA-Z]{5,50}$/;
+
+  if(!regExp.test(txtNombreGrupo))
+  {
+    bValido = false;
+    let smallError=frmAñadirGrupo.txtNombreGrupo.nextElementSibling;
+    smallError.textContent= "Formato del grupo incorrecto. (De 5-50 caracteres)";
+  }
+  else
+  {
+    let smallError=frmAñadirGrupo.txtNombreGrupo.nextElementSibling;
+    smallError.textContent= "";
+  }
+  
+  let oListadoUsuarios = frmAñadirGrupo.usuariosGrupos;
+  let oSeleccionado = oListadoUsuarios.selectedOptions;
+  let nombreSeleccionado = []
+
+
+  if(oSeleccionado.length == 0)
+  {
+    bValido = false;
+    let smallError=frmAñadirGrupo.usuariosGrupos.nextElementSibling;
+    smallError.textContent= "Incluya algún usuario en el grupo";
+  }
+  else
+  {
+    let smallError=frmAñadirGrupo.usuariosGrupos.nextElementSibling;
+    smallError.textContent= "";
+    for (let i=0;i<oSeleccionado.length;i++)
+    {
+      nombreSeleccionado.push(oSeleccionado[i].textContent);
+    }
+  }
+
+  if(bValido)
+  {
+
+    let oListadoUsuarios = oNotas.getUsuarios();
+    let oListadoObjetosUsuario = [];
+//CREO UN ARRAY QUE BUSQUE LOS USUARIOS QUE SE HAN SELECCIONADO EN EL ARRAY DE USUARIO, SI LO ENCUENTRA METE EL OBJETO DE USUARIO//
+    for(let i=0;i<oListadoUsuarios.length;i++)
+    {
+      let nombreAEncontrar = oListadoUsuarios.find(oU => oU.usuario == nombreSeleccionado[i])
+      if(nombreAEncontrar)
+      {
+        oListadoObjetosUsuario.push(nombreAEncontrar);
+      }
+    }
+
+
+    let oGrupo = new Grupo(txtNombreGrupo, oListadoObjetosUsuario);
+
+
+    let mensaje=frmAñadirGrupo.firstElementChild;
+    mensaje.classList.remove("ocultar");
+    if(oNotas.altaGrupo(oGrupo))
+    {
+      mensaje.textContent="Grupo creado correctamente.";
+      mensaje.classList.remove("alert-danger");
+      mensaje.classList.add("alert-success");
+      generarTablaGrupos();
+      frmAñadirGrupo.reset();
+    }
+    else
+    {
+      mensaje.textContent="Ese grupo ya esta en nuestra plataforma";
+      mensaje.classList.remove("alert-success");
+      mensaje.classList.add("alert-danger");
+    }
+  }
+}
+
+
+  function generarTablaGrupos()
+  {
+  
+    //vaciar tablas si es q hay
+    let posibleTabla=document.querySelector("#contenedor-grupos table");
+    if(posibleTabla!=null)
+    {
+      posibleTabla.remove();
+    }
+  
+    //creación de tabla
+    let tabla=document.createElement("TABLE");
+    tabla.classList.add("table");
+  
+    let thead=tabla.createTHead();
+    let row1=thead.insertRow(-1);
+    row1.insertCell(-1).textContent="NOMBRE DEL GRUPO";
+    row1.insertCell(-1).textContent="MIEMBROS DEL GRUPO";
+    row1.insertCell(-1).textContent="EDITAR";
+    row1.insertCell(-1).textContent="ELIMINAR";
+  
+    //cuerpo de la tabla
+    let tbody=tabla.createTBody();
+  
+    oNotas.generarFilasGrupos(tbody);
+  
+
+    let contGrupos=document.getElementById("contenedor-grupos");
+  
+  
+    contGrupos.insertBefore(tabla,contGrupos.lastElementChild);
+  }
+
+
+
+
+
+
+
+
+//EVENTOS
+
+//registrar usuario
+let $btnRegistrar = document.getElementById("btnRegistrarUsuario");
+
+$btnRegistrar.addEventListener("click", registrarUsuario);
+
+let $btnCerrarRegistro=document.querySelector("#modalRegistroUsuario button");
+
+$btnCerrarRegistro.addEventListener("click", limpiarRegistro);
+
+//borrar usuario
+let $contenedorUsuario=document.getElementById("contenedor-usuarios");
+$contenedorUsuario.addEventListener("click",borrarUsuario);
+
+//LOGIN
+let $btnEntrarLogin = document.getElementById("btnModalLogin");
+
+$btnEntrarLogin.addEventListener("click", iniciarSesion);
+
+let $btnCerrarLogin = document.getElementById("btnCerrarModalLogin");
+
+$btnCerrarLogin.addEventListener("click", limpiarLogin);
+
+//GRUPOS//
+let btnAñadirGrupo = document.querySelector("#btnCrearGrupos");
+btnAñadirGrupo.addEventListener("click", validarGrupos);
+ 
+let btnGenerarTablaGrupos = document.querySelector("#generarTablaGrupos");
+btnGenerarTablaGrupos.addEventListener("click", generarTablaGrupos);
--- js/objetos.js
+++ js/objetos.js
@@ -1,40 +1,195 @@
-class Notas{
-    constructor(){
-        this._notas=[];
-        this._usuarios=[];
-        this._usuarioGrupos=[];
-        this._grupos=[];
+'use strict';
+class Notas {
+    constructor() {
+        this._notas = [];
+        this._usuarios = [];
+        this._usuarioGrupos = [];
+        this._grupos = [];
     }
 
-    registrarUsuario(oUsuario){
-
+    getUsuarios()
+    {
+        return this._usuarios;
     }
 
-    modificarUsuario(oUsuario){
+    registrarUsuario(oUsuario) 
+    {
+        let bResultado = true;
 
-    }
-
-    bajaUsuario(oUsuario){
-
-    }
-
-    altaNota(oNota){
-
-    }
-
-    modificarNota(oNota){
-
-    }
-
-    bajaNota(oNota){
-
-    }
-
-    altaGrupo(oGrupo){
+        if(this._usuarios.some(oU => (oU.usuario == oUsuario.usuario || oU.email == oUsuario.email)))
+        {
+            bResultado = false;
+        }
+        else
+        {
+            this._usuarios.push(oUsuario);
+        }
+        return bResultado;
 
     }
 
-    bajaGrupo(oGrupo){
+    buscarUsuario(sNombreUsuario,sPassword){
+        //buscar usuario, false si no lo encuentra, true si sí
+        return true;
+    }
 
+    modificarUsuario(oUsuario) {
+
+    }
+
+    bajaUsuario(sNombreUsuario) {
+        let iPosUsuario=this._usuarios.findIndex(oUsuario=>oUsuario.usuario==sNombreUsuario);
+        this._usuarios.splice(iPosUsuario,1);
+
+        let iPosUsuarioGrupo=this._usuarioGrupos.findIndex(oGrupoUsuario=>oGrupoUsuario.idUsuario==sNombreUsuario);
+        //en caso de que el usuario pertenezca a un grupo
+        if(iPosUsuarioGrupo!=-1){
+            this._usuarioGrupos.splice(iPosUsuarioGrupo,1);
+        }
+    }
+
+    generarFilasUsuarios($tbody){
+        
+        for(let u of this._usuarios){
+            let $row=$tbody.insertRow(-1);
+            $row.insertCell(-1).textContent=u.usuario;
+            $row.insertCell(-1).textContent=u.nombre;
+            $row.insertCell(-1).textContent=u.email;
+            $row.insertCell(-1).textContent=u.contraseña;
+            
+            
+
+            let $editar=document.createElement("div");
+            $editar.classList.add("fondo-editar");
+            $editar.dataset.id=u.usuario;
+            $editar.dataset.toggle="modal";
+            $editar.dataset.target="#modalEditarUsuario";
+
+            $row.insertCell(-1).appendChild($editar);
+
+            let $eliminar=document.createElement("div");
+            $eliminar.classList.add("fondo-eliminar");
+            $eliminar.dataset.id=u.usuario;
+            $row.insertCell(-1).appendChild($eliminar);
+        }
+    }
+
+
+
+    altaNota(oNota) 
+    {
+
+    }
+
+    modificarNota(oNota) {
+
+    }
+
+    bajaNota(oNota) {
+
+    }
+
+    altaGrupo(oGrupo) 
+    {
+        let bResultado = true;
+
+        if(this._grupos.some(oG => (oG.nombreGrupo == oGrupo.sNombreGrupo)))
+        {
+            bResultado = false;
+        }
+        else
+        {
+            this._grupos.push(oGrupo);
+        }
+        return bResultado;
+
+    }
+
+    bajaGrupo(oGrupo) {
+
+    }
+
+    generarFilasGrupos(tBody)
+    {
+        
+        
+        for(let g of this._grupos)
+        {
+            console.log(g.listaUsuarios);
+            let nombreUsuarios = [];
+            for(let oU of g.listaUsuarios){
+                nombreUsuarios.push(oU.usuario);
+            }
+            
+            let $row=tBody.insertRow(-1); 
+            $row.insertCell(-1).textContent=g.nombreGrupo
+            
+
+            //agregar usuarios
+            let oLista = document.createElement("ul");
+        for(let i=0;i<nombreUsuarios.length;i++)
+        {
+            console.log(nombreUsuarios[i]);
+            let oElementoList = document.createElement("li");
+            oElementoList.textContent = nombreUsuarios[i];
+            oLista.appendChild(oElementoList);
+        }
+
+        $row.insertCell(-1).appendChild(oLista);
+            //editar y eliminar
+            let $editar=document.createElement("div");
+            $editar.classList.add("fondo-editar");
+            $editar.dataset.id=g.nombreGrupo;
+            $editar.dataset.toggle="modal";
+            $editar.dataset.target="#modalEditarGrupo";
+
+            $row.insertCell(-1).appendChild($editar);
+
+            let $eliminar=document.createElement("div");
+            $eliminar.classList.add("fondo-eliminar");
+            $eliminar.dataset.id=g.nombreGrupo;
+            $row.insertCell(-1).appendChild($eliminar);
+
+        }
+    }
+}
+
+class Usuario {
+    constructor(sUsuario, sContraseña, sEMail, sNombre) {
+        this.usuario = sUsuario;
+        this.contraseña = sContraseña;
+        this.email = sEMail;
+        this.nombre = sNombre;
+        this.notas = [];
+    }
+
+}
+
+class Nota
+{
+    constructor(iIdNota, sTitulo, sContenido)
+    {
+        this.idNota = iIdNota;
+        this.titulo = sTitulo;
+        this.contenido = sContenido;
+    }
+}
+
+class Grupo 
+{
+    constructor(sNombreGrupo, arrlistaUsuarios)
+    {
+        this.nombreGrupo = sNombreGrupo;
+        this.listaUsuarios = arrlistaUsuarios;
+    }
+}
+
+
+class UsuarioGrupo
+{
+    constructor(sIdUsuario, sIdGrupo)
+    {
+        this.idUsuario = sIdUsuario;
+        this.idGrupo = sIdGrupo;
     }
 }
