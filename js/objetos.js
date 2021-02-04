4c5435dfffa69778bab0e9df7186c37b2e7b5c56
Bruno <bruno259825@gmail.com>
2021-02-04 10:10:50 +0100

    sin cambios

--- js/objetos.js
+++ js/objetos.js
@@ -24,144 +24,144 @@ class Notas {
     }
 
 
-}
 
 
-buscarUsuario(sNombreUsuario) {
+
+    buscarUsuario(sNombreUsuario) {
 
 
-    let oUsuario = this._usuarios.find(oUser => oUser.usuario == sNombreUsuario);
+        let oUsuario = this._usuarios.find(oUser => oUser.usuario == sNombreUsuario);
 
-    return oUsuario;
-}
-
-modificarUsuario(oUsuario) {
-    //comprobar que el email no exista. 
-    let bModificado = true;
-
-    if (this._usuarios.some(oU => (oU.email == oUsuario.email) && (oUsuario.usuario != oU.usuario))) {
-        bModificado = false;
-    } else {
-        let oRefeUsuario = this.buscarUsuario(oUsuario.usuario);
-
-        oRefeUsuario.email = oUsuario.email;
-        oRefeUsuario.nombre = oUsuario.nombre;
-        oRefeUsuario.contraseña = oUsuario.contraseña;
+        return oUsuario;
     }
 
-    return bModificado;
-}
+    modificarUsuario(oUsuario) {
+        //comprobar que el email no exista. 
+        let bModificado = true;
 
-bajaUsuario(sNombreUsuario) {
-    let iPosUsuario = this._usuarios.findIndex(oUsuario => oUsuario.usuario == sNombreUsuario);
-    this._usuarios.splice(iPosUsuario, 1);
+        if (this._usuarios.some(oU => (oU.email == oUsuario.email) && (oUsuario.usuario != oU.usuario))) {
+            bModificado = false;
+        } else {
+            let oRefeUsuario = this.buscarUsuario(oUsuario.usuario);
 
-    let iPosUsuarioGrupo = this._usuarioGrupos.findIndex(oGrupoUsuario => oGrupoUsuario.idUsuario == sNombreUsuario);
-    //en caso de que el usuario pertenezca a un grupo
-    if (iPosUsuarioGrupo != -1) {
-        this._usuarioGrupos.splice(iPosUsuarioGrupo, 1);
-    }
-}
-
-generarFilasUsuarios($tbody) {
-
-    for (let u of this._usuarios) {
-        let $row = $tbody.insertRow(-1);
-        $row.insertCell(-1).textContent = u.usuario;
-        $row.insertCell(-1).textContent = u.nombre;
-        $row.insertCell(-1).textContent = u.email;
-        $row.insertCell(-1).textContent = u.contraseña;
-
-
-
-        let $editar = document.createElement("div");
-        $editar.classList.add("fondo-editar");
-        $editar.dataset.id = u.usuario;
-        $editar.dataset.toggle = "modal";
-        $editar.dataset.target = "#modalEditarUsuario";
-
-        $row.insertCell(-1).appendChild($editar);
-
-        let $eliminar = document.createElement("div");
-        $eliminar.classList.add("fondo-eliminar");
-        $eliminar.dataset.id = u.usuario;
-        $row.insertCell(-1).appendChild($eliminar);
-    }
-}
-
-
-
-altaNota(oNota, sIdUsuario) {
-    let oUsuario = this._usuarios.find(oU => oU.usuario == sIdUsuario);
-    oUsuario.notas.push(oNota);
-    this._notas.push(oNota);
-}
-
-modificarNota(oNota) {
-
-}
-
-bajaNota(oNota) {
-
-}
-
-altaGrupo(oGrupo) {
-    this._grupos.push(oGrupo);
-}
-
-bajaGrupo(oGrupo) {
-
-}
-
-generarFilasGrupos(tBody) {
-
-    for (let g of this._grupos) {
-        // busca todos los usuarios de un grupo
-        let listaGruposUsuario = this._usuarioGrupos.filter(oUsuGru => g.id == oUsuGru.idGrupo);
-        let oUsuarios = [];
-        for (let i = 0; i < listaGruposUsuario.length; i++) {
-            let oUsuario = this.buscarUsuario(listaGruposUsuario[i].idUsuario);
-            oUsuarios.push(oUsuario);
+            oRefeUsuario.email = oUsuario.email;
+            oRefeUsuario.nombre = oUsuario.nombre;
+            oRefeUsuario.contraseña = oUsuario.contraseña;
         }
 
-        let $row = tBody.insertRow(-1);
-        $row.insertCell(-1).textContent = g.id;
-        $row.insertCell(-1).textContent = g.nombreGrupo;
+        return bModificado;
+    }
 
-        let oLista = document.createElement("ul");
+    bajaUsuario(sNombreUsuario) {
+        let iPosUsuario = this._usuarios.findIndex(oUsuario => oUsuario.usuario == sNombreUsuario);
+        this._usuarios.splice(iPosUsuario, 1);
 
-        for (let i = 0; i < oUsuarios.length; i++) {
-            let oElementoList = document.createElement("li");
-            oElementoList.textContent = oUsuarios[i].usuario;
-            oLista.appendChild(oElementoList);
+        let iPosUsuarioGrupo = this._usuarioGrupos.findIndex(oGrupoUsuario => oGrupoUsuario.idUsuario == sNombreUsuario);
+        //en caso de que el usuario pertenezca a un grupo
+        if (iPosUsuarioGrupo != -1) {
+            this._usuarioGrupos.splice(iPosUsuarioGrupo, 1);
+        }
+    }
+
+    generarFilasUsuarios($tbody) {
+
+        for (let u of this._usuarios) {
+            let $row = $tbody.insertRow(-1);
+            $row.insertCell(-1).textContent = u.usuario;
+            $row.insertCell(-1).textContent = u.nombre;
+            $row.insertCell(-1).textContent = u.email;
+            $row.insertCell(-1).textContent = u.contraseña;
+
+
+
+            let $editar = document.createElement("div");
+            $editar.classList.add("fondo-editar");
+            $editar.dataset.id = u.usuario;
+            $editar.dataset.toggle = "modal";
+            $editar.dataset.target = "#modalEditarUsuario";
+
+            $row.insertCell(-1).appendChild($editar);
+
+            let $eliminar = document.createElement("div");
+            $eliminar.classList.add("fondo-eliminar");
+            $eliminar.dataset.id = u.usuario;
+            $row.insertCell(-1).appendChild($eliminar);
+        }
+    }
+
+
+
+    altaNota(oNota, sIdUsuario) {
+        let oUsuario = this._usuarios.find(oU => oU.usuario == sIdUsuario);
+        oUsuario.notas.push(oNota);
+        this._notas.push(oNota);
+    }
+
+    modificarNota(oNota) {
+
+    }
+
+    bajaNota(oNota) {
+
+    }
+
+    altaGrupo(oGrupo) {
+        this._grupos.push(oGrupo);
+    }
+
+    bajaGrupo(oGrupo) {
+
+    }
+
+    generarFilasGrupos(tBody) {
+
+        for (let g of this._grupos) {
+            // busca todos los usuarios de un grupo
+            let listaGruposUsuario = this._usuarioGrupos.filter(oUsuGru => g.id == oUsuGru.idGrupo);
+            let oUsuarios = [];
+            for (let i = 0; i < listaGruposUsuario.length; i++) {
+                let oUsuario = this.buscarUsuario(listaGruposUsuario[i].idUsuario);
+                oUsuarios.push(oUsuario);
+            }
+
+            let $row = tBody.insertRow(-1);
+            $row.insertCell(-1).textContent = g.id;
+            $row.insertCell(-1).textContent = g.nombreGrupo;
+
+            let oLista = document.createElement("ul");
+
+            for (let i = 0; i < oUsuarios.length; i++) {
+                let oElementoList = document.createElement("li");
+                oElementoList.textContent = oUsuarios[i].usuario;
+                oLista.appendChild(oElementoList);
+            }
+
+            $row.insertCell(-1).appendChild(oLista);
+
+            //editar y eliminar
+            let $editar = document.createElement("div");
+            $editar.classList.add("fondo-editar");
+            $editar.dataset.id = g.id;
+            $editar.dataset.toggle = "modal";
+            $editar.dataset.target = "#modalEditarGrupo";
+
+            $row.insertCell(-1).appendChild($editar);
+
+            let $eliminar = document.createElement("div");
+            $eliminar.classList.add("fondo-eliminar");
+            $eliminar.dataset.id = g.id;
+            $row.insertCell(-1).appendChild($eliminar);
         }
 
-        $row.insertCell(-1).appendChild(oLista);
-
-        //editar y eliminar
-        let $editar = document.createElement("div");
-        $editar.classList.add("fondo-editar");
-        $editar.dataset.id = g.id;
-        $editar.dataset.toggle = "modal";
-        $editar.dataset.target = "#modalEditarGrupo";
-
-        $row.insertCell(-1).appendChild($editar);
-
-        let $eliminar = document.createElement("div");
-        $eliminar.classList.add("fondo-eliminar");
-        $eliminar.dataset.id = g.id;
-        $row.insertCell(-1).appendChild($eliminar);
     }
 
-}
+    altaUsuarioGrupo(oGrupo, oListadoObjetosUsuario) {
 
-altaUsuarioGrupo(oGrupo, oListadoObjetosUsuario) {
+        for (let oU of oListadoObjetosUsuario) {
+            this._usuarioGrupos.push(new UsuarioGrupo(oU.usuario, oGrupo.id, ""));
+        }
 
-    for (let oU of oListadoObjetosUsuario) {
-        this._usuarioGrupos.push(new UsuarioGrupo(oU.usuario, oGrupo.id, ""));
     }
-
-}
 }
 
 class Usuario {
